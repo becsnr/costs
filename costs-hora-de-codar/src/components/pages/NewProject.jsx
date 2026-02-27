4c0906986f2ae918fe7e75354e3fbb9c361ee8cd
Beca <234265987+becsnr@users.noreply.github.com>
2026-02-27 10:03:55 -0300

    mensagens do sistema

--- costs-hora-de-codar/src/components/pages/NewProject.jsx
+++ costs-hora-de-codar/src/components/pages/NewProject.jsx
@@ -22,7 +22,9 @@ function NewProject() {
             .then((resp) => resp.json())
             .then((data) => {
                 console.log(data)
-                navigate('/projects', {message: 'Projeto criado com sucesso!'})
+                navigate('/projects', { 
+                    state: {message: "Projeto criado com sucesso!"}
+                })
             })
             .catch(err => console.log(err))
     }
