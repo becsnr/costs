8a26b64dc05dcad64a8eeba4433e2fe5956b4c37
Beca <234265987+becsnr@users.noreply.github.com>
2026-03-23 14:15:16 -0300

    remover serviços

--- costs-hora-de-codar/src/components/pages/Project.jsx
+++ costs-hora-de-codar/src/components/pages/Project.jsx
@@ -108,7 +108,33 @@ function Project() {
 
     }
 
-    function removeService() {
+    function removeService(id, cost) {
+
+        const servicesUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+
+        const projectUpdated = project;
+
+        projectUpdated.services = servicesUpdated;
+
+        // remover o custo do serviço do custo do projeto
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdated)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+        })
+        .catch(err => console.log(err))
 
     }
 
